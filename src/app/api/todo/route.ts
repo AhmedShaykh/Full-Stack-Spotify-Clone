618870f50243414d6a42f5de39e58a7ad394a5c8
Ahmed Saleem Shaikh <ahmed.shaykhwork@gmail.com>
2023-05-16 22:52:06 +0500

    Add POST Method

--- src/app/api/todo/route.ts
+++ src/app/api/todo/route.ts
@@ -1,5 +1,5 @@
 import { NextRequest, NextResponse } from "next/server";
-import { db } from '@vercel/postgres';
+import { QueryResult, db } from '@vercel/postgres';
 
 export async function GET(request: NextRequest) {
 
@@ -9,7 +9,11 @@ export async function GET(request: NextRequest) {
 
         await client.sql`CREATE TABLE IF NOT EXISTS TODOS(ID serial, TASK varchar(255))`;
 
-        return NextResponse.json({ message: "Call API" });
+        const res: QueryResult = await client.sql`SELECT * FROM TODOS`;
+
+        console.log(res.rows.find(item => item.id === 4));
+
+        return NextResponse.json({ response: res });
 
     } catch (err) {
 
@@ -19,4 +23,36 @@ export async function GET(request: NextRequest) {
 
     }
 
+};
+
+export async function POST(request: NextRequest) {
+
+    const client = await db.connect();
+
+    const reqData = await request.json();
+
+    try {
+
+        if (reqData.TASK) {
+
+            const res: QueryResult = await client.sql`INSERT INTO Todos(Task) VALUES(${reqData.TASK})`;
+
+            console.log(res);
+
+            return NextResponse.json({ message: "Data Added SuccessFully" });
+
+        }
+        else {
+
+            throw new Error("Task Field is Required!");
+
+        }
+
+    } catch (error) {
+
+        return NextResponse.json(
+            { message: (error as { message: string }).message }
+        );
+
+    }
 };
