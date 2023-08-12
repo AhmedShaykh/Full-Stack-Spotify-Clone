b754718c108f6710475638485349cf79bac1756f
Ahmed Saleem Shaikh <ahmed.shaykhwork@gmail.com>
2023-08-12 21:23:48 +0500

    Add SQL

--- next.config.js
+++ next.config.js
@@ -2,13 +2,23 @@
 
 const nextConfig = {
     images: {
-        domains: ["misc.scdn.co",
+        domains: [
+            "misc.scdn.co",
             "i.scdn.co",
             "geo-media.beatsource.com",
             "i1.sndcdn.com",
             "media.pitchfork.com",
             "seed-mix-image.spotifycdn.com",
-            "tbvswgcbrilqlaxdsxii.supabase.co"]
+            "tbvswgcbrilqlaxdsxii.supabase.co"
+        ]
+    },
+    env: {
+        NEXT_PUBLIC_SUPABASE_URL: "https://vgmtzakxbpwcyybilxdn.supabase.co",
+        NEXT_PUBLIC_SUPABASE_ANON_KEY: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZnbXR6YWt4YnB3Y3l5YmlseGRuIiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTE4NTU5MzgsImV4cCI6MjAwNzQzMTkzOH0.X4MjKMMxNANM5KY0EO8BLNgNb-P8sdGqpK0y5vVfW2s",
+        SUPABASE_SERVICE_ROLE_KEY: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZnbXR6YWt4YnB3Y3l5YmlseGRuIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTY5MTg1NTkzOCwiZXhwIjoyMDA3NDMxOTM4fQ.9L-QHukBCSkPWErsB4gsz49NWe-ktDoJ-XyocMIYoFU",
+        NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY: "",
+        STRIPE_SECRET_KEY: "",
+        STRIPE_WEBHOOK_SECRET: ""
     }
 };
 
