d92c7c0991ed16363c2372012b81b594ecfaacf1
Ahmed Saleem Shaikh <ahmed.shaykhwork@gmail.com>
2023-08-22 16:44:15 +0500

    Add Actions

---
+++ src/actions/getSongsByTitle.ts
@@ -0,0 +1,32 @@
+import { createServerComponentClient } from "@supabase/auth-helpers-nextjs";
+import { Song } from "../../Types";
+import getSongs from "./getSongs";
+import { cookies } from "next/headers";
+
+const getSongsByTitle = async (title: string): Promise<Song[]> => {
+
+    const supabase = createServerComponentClient({
+        cookies: cookies
+    });
+
+    if (!title) {
+
+        const allSongs = await getSongs();
+
+        return allSongs;
+    }
+
+    const { data, error } = await supabase
+        .from("songs")
+        .select("*")
+        .ilike("title", `%${title}%`)
+        .order("created_at", { ascending: false })
+
+    if (error) {
+        console.log(error.message);
+    }
+
+    return (data as any) || [];
+};
+
+export default getSongsByTitle;
