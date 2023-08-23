b5f5cf0f9bdb50187fb6c9d44272a7568ed33136
Ahmed Saleem Shaikh <ahmed.shaykhwork@gmail.com>
2023-08-23 16:51:48 +0500

    Final Commit

--- src/Providers/SupabaseProvider.tsx
+++ src/Providers/SupabaseProvider.tsx
@@ -1,6 +1,5 @@
 "use client";
 import { FC, useState } from "react";
-// import { Database } from "../../Types_DB";
 import { createClientComponentClient } from "@supabase/auth-helpers-nextjs";
 import { SessionContextProvider } from "@supabase/auth-helpers-react";
 
